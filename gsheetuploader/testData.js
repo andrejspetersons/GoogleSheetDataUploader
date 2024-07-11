3fab6a7970befc0e2a3d82616bc46065bb93636c
andrejspetersons <andrejpeterson1@gmail.com>
2024-07-11 17:46:40 +0300

    Commit changes 11.07.24

---
+++ gsheetuploader/testData.js
@@ -0,0 +1,39 @@
+module.exports=[
+  {
+    Name: 'TestName1',
+    LastName: 'TestLastName1',
+    Age: '20',
+    UserID: '114546767'
+  },
+  {
+    Name: 'TestName2',
+    LastName: 'TestLastName2',
+    Age: '25',
+    UserID: '224557678',
+	  Comment: 'New client'
+  },
+  {
+    Name: 'TestName3',
+    LastName: 'TestLastName3',
+    Age: '28',
+    UserID: '336787987'
+  },
+  {
+    Name: 'TestName4',
+    LastName: 'TestLastName4',
+    Age: '19',
+    UserID: '446567887'
+  },
+  {
+    Name: 'TestName5',
+    LastName: 'TestLastName5',
+    Age: '43',
+    UserID: '555686678'
+  },
+  {
+    Name: 'TestName6',
+    LastName: 'TestLastName6',
+    Age: '42',
+    UserID: '555686678'
+  }
+]
